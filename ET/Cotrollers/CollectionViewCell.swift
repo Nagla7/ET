947a6991bc11156565da30fd463534182fd73123
Najla7 <32482928+Nagla7@users.noreply.github.com>
2018-02-05 10:23:23 +0300

    Filter

---
+++ ET/Cotrollers/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ET
+//
+//  Created by njoool  on 05/02/2018.
+//  Copyright © 2018 com.GP.ET. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
