a9e9232d82e6c45680358d06a5a615a1cd96ab14
reemalz <reem-rock3@hotmail.com>
2018-02-13 10:20:30 +0300

    sign up page fully implemented

--- ET/Cotrollers/RegisterViewController.swift
+++ ET/Cotrollers/RegisterViewController.swift
@@ -25,6 +25,8 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
     @IBOutlet weak var username: HoshiTextField!
     @IBOutlet weak var password: HoshiTextField!
     @IBOutlet weak var repassword: HoshiTextField!
+    private var currentTextField: UITextField?
+    var valids = Array(repeating: true, count: 11)
     
     @IBOutlet weak var C_SP: CustomControl!
     var ref : DatabaseReference!
@@ -38,7 +40,7 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
         phone.delegate = self
         username.delegate = self
         password.delegate = self
-        
+        repassword.delegate = self
         companyName.delegate = self
         CommercialRecord.delegate = self
         
@@ -60,6 +62,9 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
         if touches.first?.view != RegisterView{self.dismiss(animated:true, completion:nil)}
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentTextField = textField
+    }
 
     
     //close button for register
@@ -95,9 +100,14 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
     
     @IBAction func createAccountAction(_ sender: Any) {
         
+        if let currentTextField = currentTextField {
+            currentTextField.resignFirstResponder()
+        }
+        
         //check which user tree
         let tree = (C_SP.selectedSegmentIndex == 0) ? "Customers":"ServiceProviders"
         var flag : Bool!
+        var flag2 : Bool!
         
         switch C_SP.selectedSegmentIndex {
         case 0: //customer
@@ -116,6 +126,17 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             present(alertController, animated: true, completion: nil)
             
         } else {
+            
+            // check all fields are valid
+            switch C_SP.selectedSegmentIndex {
+            case 0: //customer
+                flag2 = valids[0] && valids[1] && valids[2] && valids[3] && valids[4] && valids[5] && valids[6] && valids[7] && valids[8]
+            case 1:
+                flag2 = valids[0] && valids[1] && valids[2] && valids[3] && valids[4] && valids[5] && valids[6] && valids[7] && valids[8] && valids[9] && valids[10]
+            default:
+                print("none")
+            }
+            if(flag2){
                     Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
                         if error == nil {
                             print("You have successfully signed up")
@@ -133,6 +154,12 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
                             self.present(alertController, animated: true, completion: nil)
                         }
                     }
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Cannot submit form with invalid info. Please make sure all fields are in correct format.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+            }
         }
     }
     
@@ -151,10 +178,14 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: fname.text) == false
             {
+                valids[0] = false
+                fname.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "First name can contain letters only.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                valids[0] = true
             }
         }
         
@@ -164,11 +195,16 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: lname.text) == false
             {
+                valids[1] = false
+                lname.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Last name can contain letters only.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                valids[1] = true
             }
+            
         }
         
         if (textField == username)
@@ -177,22 +213,31 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: username.text) == false
             {
+                valids[2] = false
+                username.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Username has to be at least 5 characters long and can contain letters and numbers.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                valids[2] = true
             }
             
             //  check username uniqueness
             userTree = (C_SP.selectedSegmentIndex == 0) ? "Customers":"ServiceProviders"
             ref.child(userTree).queryOrdered(byChild: "username").queryEqual(toValue: username.text!.lowercased()).observeSingleEvent(of: .value , with: { snapshot in
                 if snapshot.exists() {
+                    self.valids[3] = false
+                    self.username.borderInactiveColor=UIColor.red
                     //error message: username already exists
                     let alertController = UIAlertController(title: "Uh oh!", message: "\(self.username.text!) is not available. Try another username.", preferredStyle: .alert)
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
                     self.present(alertController, animated: true, completion: nil)
-                }})
+                } else {
+                    self.valids[3] = true
+                }
+            })
             
         }
         
@@ -202,10 +247,14 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: password.text) == false
             {
+                valids[4] = false
+                password.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Password has to be at least 6 characters long and can contain letters, numbers and special characters ( . _ % @ + - )", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[4] = true
             }
         }
         
@@ -215,17 +264,26 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: repassword.text) == false
             {
+                valids[5] = false
+                repassword.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Password has to be at least 6 characters long and can contain letters, numbers and special characters ( . _ % @ + - )", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[5] = true
             }
+
             
             if !(repassword.text == password.text){
+                valids[6] = false
+                repassword.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Uh oh!", message: "Passwords don't match! Please re-enter matching passwords.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[6] = true
             }
         }
         
@@ -235,11 +293,16 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: email.text) == false
             {
+                valids[7] = false
+                email.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Enter the E-mail in correct format. e.g. example@domain.com ", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[7] = true
             }
+
         }
         
         if (textField == phone)
@@ -248,23 +311,31 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: phone.text) == false
             {
+                valids[8] = false
+                phone.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Phone number has to be 10 digits long.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[8] = true
             }
         }
         
         if (textField == companyName)
         {
-            let name_reg = "[A-Za-z0-9]{1,30}"
+            let name_reg = "[A-Za-z0-9]{1,50}"
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: companyName.text) == false
             {
+                valids[9] = false
+                companyName.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Company name can contain letters and numbers only.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[9] = true
             }
         }
         
@@ -274,10 +345,14 @@ class RegisterViewController: UIViewController , UITextFieldDelegate {
             let name_test = NSPredicate(format: "SELF MATCHES %@", name_reg)
             if name_test.evaluate(with: CommercialRecord.text) == false
             {
+                valids[10] = false
+                CommercialRecord.borderInactiveColor=UIColor.red
                 let alert = UIAlertController(title: "Format Error", message: "Commercial record has to be 10 digits long.", preferredStyle: .alert)
                 let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
                 alert.addAction(ok)
                 self.present(alert, animated: true, completion: nil)
+            } else {
+                self.valids[10] = true
             }
         }
         
